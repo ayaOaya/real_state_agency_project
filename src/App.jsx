2d9a5a0c88e1a7fa1a5de4bc673a1a7af4090d81
ayaOaya <alyaauaacn@gmail.com>
2023-03-22 23:00:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Lenis from '@studio-freight/lenis';
+/* import { BrowserRouter, Routes, Route } from "react-router-dom";*/
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+  direction: 'vertical',
+  gestureDirection: 'vertical', 
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <About />
+    <Footer />
+
+   {/*  <BrowserRouter>
+     <Routes>
+    <Route path="/" element={<Navbar />} />
+    <Route path="/header" element={<Header />} />
+    <Route path="/about" element={ <About />} />
+    <Route path="/footer" element={<Footer />} />
+    </Routes>
+    </BrowserRouter> */}
+    
+    </>
+  )
+}
+
+export default App
