f7bba10947f1a3552906a67718f5249ee3e80940
ayaOaya <alyaauaacn@gmail.com>
2023-03-23 16:27:35 +0100

    third commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import "./style.scss"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+    <div className="header">
+
+      <div className="title-flex">
+        <div className="box"></div>
+        <h1 className="title-flex-h1">Company name</h1>
+      </div>
+
+      <div className="border"></div>
+
+      <div className="header-bg-img"></div>
+      <div className="wrapper">
+        <div className="letters">
+          <div className="sm-box-hor"></div>
+          <div className="sm-box-ver"></div>
+          <div className="a">A</div>
+          <div className="g">G</div>
+          <div className="circle"></div>
+          <div className="n">N</div>
+
+        </div>
+      </div>
+
+      <div className="bottom-conatainer">
+
+        <div className="bottom-btns">
+          <button className="btn1"><a href="/">Prfolio projects</a></button>
+          <button className='btn2'><a href="/">About</a></button>
+        </div>
+
+        <div className="bottom-texts">
+          <p className="p1">Lorem ipsum,  elit. Magni harum numquam tempora a, aliquam ipsam maiores obcaecati, exercitationem itaque delectus incidunt cumque optio sequi, sint nisi earum .</p>
+
+          <p className="p2">Lorem ipsum  consectetur  elit. Magnam, asperiores, earum nihil accusantium  deserunt voluptatum rerum minus amet eos ratione eaque </p>
+        </div>
+      </div>
+      
+    </div>
+
+    </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import "./style.scss"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="navbar">
+    <h1 className="title">title</h1>
+    <div className="links-container1">
+      <ul className="ul-container1">
+        <li className="li-container1"><a href="/">Home</a></li>
+        <li className="li-container1"><a href="/">About</a></li>
+        <li className="li-container1"><a href="/">Info</a></li>
+        <li className="li-container1"><a href="/">Blog</a></li>
+      </ul>
+    </div>
+
+    <div className="links-container2">
+      <ul className="ul-container2">
+        <li className="li-container2"><a href="/">Contact</a></li>
+        <li className="li-container2"><a href="/">Layer</a></li>
+        <li className="li-container2"><a href="/">Company</a></li>
+        <li className="li-container2"><a href="/">Location</a></li>
+      </ul>
+    </div>
+    </div>
+    </>
   )
 }
